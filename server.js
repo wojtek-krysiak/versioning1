2c49e5bc63e82393dca5bfbd9f0204af33eea4fe
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-06-30 13:20:34 +0200

    fix bug

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(
-  `Example app listening at http://localhost:${port}`,
+  `Example app listening at http://127.0.0.1:${port}`,
 ));
