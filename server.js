f59b69b3de8db972b6bc2eeae18fe04fdc10616f
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-07-29 14:53:51 +0200

    bring back old version

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/', (req, res) => res.send('Hello World2!'));
 
 app.listen(port, () => console.log(
   `Example app listening at http://127.0.0.1:${port}`,
