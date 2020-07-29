ac6fb9f53d048e64240bb8d4385dc9f10488e88d
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-07-29 13:51:53 +0200

    bring back old code

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/hello2', (req, res) => res.send('Hello World 2!'));
-
 app.listen(port, () => console.log(
   `Example app listening at http://127.0.0.1:${port}`,
 ));
